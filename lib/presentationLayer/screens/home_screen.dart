cf0e2bccd95e09576bf2253d4283380e2a71767c
smarttech <mmanik2548@gmail.com>
2024-06-02 18:01:02 +0600

    add bottomNavigationBar

--- lib/presentationLayer/screens/home_screen.dart
+++ lib/presentationLayer/screens/home_screen.dart
@@ -6,8 +6,70 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Doken'),),
-      body: Container(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Dokan",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black45,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.search),
+        backgroundColor: Colors.deepOrange,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        // shape: BeveledRectangleBorder(
+        //     // borderRadius: BorderRadius.circular(20.0),
+        //     // side: BorderSide(color: Colors.blue, width: 2.0, style: BorderStyle.solid)
+        //     ),
+        // mini: true,
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        height: 80,
+        notchMargin: 5.0,
+        shape: CircularNotchedRectangle(),
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Icon(
+                Icons.home,
+                color: Color(0xff6E7FAA),
+              ),
+            ),
+            Padding(
+              padding:
+              EdgeInsets.only(right: 20.0, top: 10.0, bottom: 10.0),
+              child: Icon(
+                Icons.shopping_cart,
+                color: Color(0xff6E7FAA),
+              ),
+            ),
+            Padding(
+              padding:
+              EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
+              child: Icon(
+                Icons.favorite,
+                color: Color(0xff6E7FAA),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: Icon(
+                Icons.settings,
+                color: Color(0xff6E7FAA),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
